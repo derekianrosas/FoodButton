7535cf5bf5611df7c773240c7899871da0cfc0f5
derek rosas <derekianrosas@gmail.com>
2020-06-17 21:42:06 -0600

    added style hidden to images

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,12 @@ import taco from "../../static/assets/SGT.jpg";
 import steak from "../../static/assets/SGC.jpg";
 import spaghetti from "../../static/assets/SGS.jpg";
 
-const foodPhotos = [{ Type: "Burger", image: burger }];
+const foodPhotos = [
+  { Type: "Burger", image: burger },
+  { Type: "Taco", image: taco },
+  { Type: "Steak", image: steak },
+  { Type: "Spaghetti", image: spaghetti },
+];
 
 export default class App extends Component {
   constructor(props) {
@@ -27,10 +32,22 @@ export default class App extends Component {
         <button>Do you like Burgers?</button>
         <button>Do you like Spaghetti?</button>
         <button>Do you like Steak?</button>
-        <img src={this.state.foodData[0].image}></img>
-        <img src={this.state.foodData[1].image}></img>
-        <img src={this.state.foodData[2].image}></img>
-        <img src={this.state.foodData[3].image}></img>
+        <img
+          src={this.state.foodData[0].image}
+          style={{ visibility: "hidden" }}
+        ></img>
+        <img
+          src={this.state.foodData[1].image}
+          style={{ visibility: "hidden" }}
+        ></img>
+        <img
+          src={this.state.foodData[2].image}
+          style={{ visibility: "hidden" }}
+        ></img>
+        <img
+          src={this.state.foodData[3].image}
+          style={{ visibility: "hidden" }}
+        ></img>
       </div>
     );
   }
