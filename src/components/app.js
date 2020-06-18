1d06f0fbb5995f1e36a2e335ff68aa14943ae56b
derek rosas <derekianrosas@gmail.com>
2020-06-17 21:32:07 -0600

    added image tags

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { Redirect } from "react-router";
 import burger from "../../static/assets/SGB.png";
+import taco from "../../static/assets/SGT.jpg";
+import steak from "../../static/assets/SGC.jpg";
+import spaghetti from "../../static/assets/SGS.jpg";
 
 const foodPhotos = [{ Type: "Burger", image: burger }];
 
@@ -25,6 +28,9 @@ export default class App extends Component {
         <button>Do you like Spaghetti?</button>
         <button>Do you like Steak?</button>
         <img src={this.state.foodData[0].image}></img>
+        <img src={this.state.foodData[1].image}></img>
+        <img src={this.state.foodData[2].image}></img>
+        <img src={this.state.foodData[3].image}></img>
       </div>
     );
   }
